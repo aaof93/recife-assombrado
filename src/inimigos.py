7e5c8075a1f3c3ec9c95eb9707146e6e08400b7f
Geraldo Camargo <camargo.geraldo@gmail.com>
2026-06-20 16:08:01 -0300

    mais imagens carregadas.

--- src/inimigos.py
+++ src/inimigos.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import os
 
 from config import COR_INIMIGO
 
@@ -97,12 +98,15 @@ class InimigoBase:
 
 
 class PernaCabeluda(InimigoBase):
+    imagem_direita_cache = None
+    imagem_esquerda_cache = None
+
     def __init__(self, x, y):
         super().__init__(
             x=x,
             y=y,
-            largura=35,
-            altura=35,
+            largura=75,
+            altura=100,
             cor=COR_INIMIGO,
             velocidade=2.6,
             dano=10,
@@ -110,14 +114,97 @@ class PernaCabeluda(InimigoBase):
             valor_xp=10
         )
 
+        self.olhando_para_direita = True
+
+        self.imagem_direita = self.carregar_imagem(
+            "perna_cabeluda_direita.png",
+            "direita"
+        )
+
+        self.imagem_esquerda = self.carregar_imagem(
+            "perna_cabeluda_esquerda.png",
+            "esquerda"
+        )
+
+    def carregar_imagem(self, nome_arquivo, lado):
+        if lado == "direita" and PernaCabeluda.imagem_direita_cache is not None:
+            return PernaCabeluda.imagem_direita_cache
+
+        if lado == "esquerda" and PernaCabeluda.imagem_esquerda_cache is not None:
+            return PernaCabeluda.imagem_esquerda_cache
+
+        pasta_atual = os.path.dirname(__file__)
+        caminho = os.path.join(pasta_atual, "assets", nome_arquivo)
+
+        try:
+            imagem = pygame.image.load(caminho)
+            imagem = imagem.convert_alpha()
+            imagem = pygame.transform.scale(
+                imagem,
+                (self.largura, self.altura)
+            )
+
+            if lado == "direita":
+                PernaCabeluda.imagem_direita_cache = imagem
+            else:
+                PernaCabeluda.imagem_esquerda_cache = imagem
+
+            return imagem
+
+        except FileNotFoundError:
+            return None
+
+    def mover(self, jogador):
+        if self.x > jogador.x:
+            self.olhando_para_direita = False
+        else:
+            self.olhando_para_direita = True
+
+        diferenca_x = jogador.x - self.x
+        diferenca_y = jogador.y - self.y
+
+        distancia = math.sqrt(diferenca_x ** 2 + diferenca_y ** 2)
+
+        if distancia != 0:
+            direcao_x = diferenca_x / distancia
+            direcao_y = diferenca_y / distancia
+
+            self.x += direcao_x * self.velocidade
+            self.y += direcao_y * self.velocidade
+
+    def desenhar(self, tela):
+        imagem_atual = None
+
+        if self.olhando_para_direita:
+            imagem_atual = self.imagem_direita
+        else:
+            imagem_atual = self.imagem_esquerda
+
+        if imagem_atual is not None:
+            tela.blit(
+                imagem_atual,
+                (self.x, self.y)
+            )
+        else:
+            pygame.draw.rect(
+                tela,
+                self.cor,
+                self.obter_retangulo()
+            )
+
+        self.desenhar_barra_vida(tela)
+
 
 class Emparedada(InimigoBase):
+    imagem_direita_cache = None
+    imagem_esquerda_cache = None
+
     def __init__(self, x, y):
         super().__init__(
             x=x,
             y=y,
-            largura=32,
-            altura=45,
+            largura=85,
+            altura=110,
             cor=(170, 170, 255),
             velocidade=1.8,
             dano=15,
@@ -129,7 +216,52 @@ class Emparedada(InimigoBase):
         self.amplitude = 40
         self.frequencia = 0.006
 
+        self.olhando_para_direita = True
+
+        self.imagem_direita = self.carregar_imagem(
+            "emparedada_direita.png",
+            "direita"
+        )
+
+        self.imagem_esquerda = self.carregar_imagem(
+            "emparedada_esquerda.png",
+            "esquerda"
+        )
+
+    def carregar_imagem(self, nome_arquivo, lado):
+        if lado == "direita" and Emparedada.imagem_direita_cache is not None:
+            return Emparedada.imagem_direita_cache
+
+        if lado == "esquerda" and Emparedada.imagem_esquerda_cache is not None:
+            return Emparedada.imagem_esquerda_cache
+
+        pasta_atual = os.path.dirname(__file__)
+        caminho = os.path.join(pasta_atual, "assets", nome_arquivo)
+
+        try:
+            imagem = pygame.image.load(caminho)
+            imagem = imagem.convert_alpha()
+            imagem = pygame.transform.scale(
+                imagem,
+                (self.largura, self.altura)
+            )
+
+            if lado == "direita":
+                Emparedada.imagem_direita_cache = imagem
+            else:
+                Emparedada.imagem_esquerda_cache = imagem
+
+            return imagem
+
+        except FileNotFoundError:
+            return None
+
     def mover(self, jogador):
+        if self.x > jogador.x:
+            self.olhando_para_direita = False
+        else:
+            self.olhando_para_direita = True
+
         diferenca_x = jogador.x - self.x
         diferenca_y = jogador.y - self.y
 
@@ -150,14 +282,39 @@ class Emparedada(InimigoBase):
             self.x += -direcao_y * movimento_senoidal
             self.y += direcao_x * movimento_senoidal
 
+    def desenhar(self, tela):
+        imagem_atual = None
+
+        if self.olhando_para_direita:
+            imagem_atual = self.imagem_direita
+        else:
+            imagem_atual = self.imagem_esquerda
+
+        if imagem_atual is not None:
+            tela.blit(
+                imagem_atual,
+                (self.x, self.y)
+            )
+        else:
+            pygame.draw.rect(
+                tela,
+                self.cor,
+                self.obter_retangulo()
+            )
+
+        self.desenhar_barra_vida(tela)
+
 
 class HomemDoSaco(InimigoBase):
+    imagem_direita_cache = None
+    imagem_esquerda_cache = None
+
     def __init__(self, x, y):
         super().__init__(
             x=x,
             y=y,
-            largura=70,
-            altura=70,
+            largura=150,
+            altura=150,
             cor=(120, 70, 35),
             velocidade=1.1,
             dano=25,
@@ -167,7 +324,52 @@ class HomemDoSaco(InimigoBase):
 
         self.eh_chefe = True
 
+        self.olhando_para_direita = True
+
+        self.imagem_direita = self.carregar_imagem(
+            "homem_saco_direita.png",
+            "direita"
+        )
+
+        self.imagem_esquerda = self.carregar_imagem(
+            "homem_saco_esquerda.png",
+            "esquerda"
+        )
+
+    def carregar_imagem(self, nome_arquivo, lado):
+        if lado == "direita" and HomemDoSaco.imagem_direita_cache is not None:
+            return HomemDoSaco.imagem_direita_cache
+
+        if lado == "esquerda" and HomemDoSaco.imagem_esquerda_cache is not None:
+            return HomemDoSaco.imagem_esquerda_cache
+
+        pasta_atual = os.path.dirname(__file__)
+        caminho = os.path.join(pasta_atual, "assets", nome_arquivo)
+
+        try:
+            imagem = pygame.image.load(caminho)
+            imagem = imagem.convert_alpha()
+            imagem = pygame.transform.scale(
+                imagem,
+                (self.largura, self.altura)
+            )
+
+            if lado == "direita":
+                HomemDoSaco.imagem_direita_cache = imagem
+            else:
+                HomemDoSaco.imagem_esquerda_cache = imagem
+
+            return imagem
+
+        except FileNotFoundError:
+            return None
+
     def mover(self, jogador):
+        if self.x > jogador.x:
+            self.olhando_para_direita = False
+        else:
+            self.olhando_para_direita = True
+
         diferenca_x = jogador.x - self.x
         diferenca_y = jogador.y - self.y
 
@@ -181,30 +383,44 @@ class HomemDoSaco(InimigoBase):
             self.y += direcao_y * self.velocidade
 
     def desenhar(self, tela):
-        pygame.draw.rect(
-            tela,
-            self.cor,
-            self.obter_retangulo()
-        )
+        imagem_atual = None
 
-        pygame.draw.rect(
-            tela,
-            (80, 40, 20),
-            (self.x + 10, self.y + 10, self.largura - 20, self.altura - 20)
-        )
+        if self.olhando_para_direita:
+            imagem_atual = self.imagem_direita
+        else:
+            imagem_atual = self.imagem_esquerda
 
-        pygame.draw.circle(
-            tela,
-            (255, 255, 255),
-            (int(self.x + 25), int(self.y + 25)),
-            4
-        )
+        if imagem_atual is not None:
+            tela.blit(
+                imagem_atual,
+                (self.x, self.y)
+            )
 
-        pygame.draw.circle(
-            tela,
-            (255, 255, 255),
-            (int(self.x + 45), int(self.y + 25)),
-            4
-        )
+        else:
+            pygame.draw.rect(
+                tela,
+                self.cor,
+                self.obter_retangulo()
+            )
+
+            pygame.draw.rect(
+                tela,
+                (80, 40, 20),
+                (self.x + 10, self.y + 10, self.largura - 20, self.altura - 20)
+            )
+
+            pygame.draw.circle(
+                tela,
+                (255, 255, 255),
+                (int(self.x + 25), int(self.y + 25)),
+                4
+            )
+
+            pygame.draw.circle(
+                tela,
+                (255, 255, 255),
+                (int(self.x + 45), int(self.y + 25)),
+                4
+            )
 
         self.desenhar_barra_vida(tela)
